569c717c231d98aeb827afddee9ade46009d99ad
Leo Lee <l1dan@hotmail.com>
2020-10-10 00:35:48 +0800

    Added: 添加 TabBarItem icons

--- UINavigationExtensionDemo/Controller/Common/DashboardTabBarController.m
+++ UINavigationExtensionDemo/Controller/Common/DashboardTabBarController.m
@@ -32,6 +32,19 @@
     
     [LightNavigationController registerViewControllerClass:[UIViewController class] forNavigationBarClass:[UINavigationBar class]];
     
+    self.tabBar.tintColor = [UIColor colorWithRed:25/255.0 green:43/255.0 blue:67/255.0 alpha:1.0];
+    
+    UITabBarItem *lightItem = self.tabBar.items[0];
+    lightItem.image = [[UIImage imageNamed:@"Light-Normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    lightItem.selectedImage = [[UIImage imageNamed:@"Light-Selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    UITabBarItem *darkItem = self.tabBar.items[1];
+    darkItem.image = [[UIImage imageNamed:@"Dark-Normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    darkItem.selectedImage = [[UIImage imageNamed:@"Dark-Selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    UITabBarItem *autoItem = self.tabBar.items[2];
+    autoItem.image = [[UIImage imageNamed:@"Auto-Normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    autoItem.selectedImage = [[UIImage imageNamed:@"Auto-Selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 }
 
 @end
